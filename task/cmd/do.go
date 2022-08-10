60a47711e6557fe65ef820470404d07bfb0c3b9a
yerassyl <y.bolatov1@gmail.com>
2022-08-10 14:51:40 +0600

    CLI shell for todo app

---
+++ task/cmd/do.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"strconv"
+)
+
+var doCmd = &cobra.Command{
+	Use:   "do",
+	Short: "Marks a task as complete",
+	Run: func(cmd *cobra.Command, args []string) {
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		fmt.Println(ids)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(doCmd)
+}
